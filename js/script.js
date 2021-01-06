6ef94550494b9db8be73e208750d10bf4742d97b
Kacper <kacperkowalski12345678@gmail.com>
2021-01-06 18:57:04 +0100

    created program

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const mark = document.querySelector('.marks')
+const countBtn = document.querySelector('.countAverage')
+const input = document.querySelector('.addMark')
+
+let marksArr = [];
+let count;
+
+const addMarks = () => {
+
+    let number = parseInt(input.value)
+    marksArr.push(number);
+
+    count = marksArr.reduce((a, b) => a + b)
+
+    mark.innerHTML = `Suma ocen: <span>${count}</span>`;
+    input.value = ''
+    input.focus()
+
+}
+const showAverage = () => {
+    if (marksArr.length !== 0) {
+        let score = count / marksArr.length;
+        mark.innerHTML = `Twoja średnia: <span>${score}</span>`;
+        marksArr = []
+    }else{
+        mark.innerHTML = 'Musisz podać ocene!'
+        input.focus()
+    }
+}
+
+input.addEventListener("keyup", addMarks)
+countBtn.addEventListener("click", showAverage)
+
+//można też zrobić to wszystko za pomocą REST OPERATORA
